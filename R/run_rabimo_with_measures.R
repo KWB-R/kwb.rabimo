3d8b2e6a5139f0fcd3148c593ff3ea4832c6204d
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2024-09-05 17:53:25 +0200

    Fix documentation or run_rabimo_with_measures()

--- R/run_rabimo_with_measures.R
+++ R/run_rabimo_with_measures.R
@@ -1,10 +1,12 @@
 #' Distribute Rainwater Management Measures and run R-Abimo
 #'
 #' @param blocks data frame of selected blocks (same columns as in
-#'   \code{link{rabimo_inputs_2020}$data})
+#'   \code{\link{rabimo_inputs_2020}$data})
 #' @param measures list with elements \code{green_roof}, \code{unpaved},
 #'   \code{to_swale} representing the target percentages of the total areas
 #'   corresponding to each measure
+#' @param config configuration object, default:
+#'   \code{\link{rabimo_inputs_2020}$config}
 #' @export
 run_rabimo_with_measures <- function(
     blocks,
