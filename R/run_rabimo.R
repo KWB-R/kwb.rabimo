2bd3ad083b01e7a80586d166c25b60d816d82081
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2025-04-03 21:22:40 +0200

    Add example for run_rabimo() (results in error!)

--- R/run_rabimo.R
+++ R/run_rabimo.R
@@ -354,6 +354,9 @@ yearly_height_to_volume_flow <- function(height, area)
 #'   returned as attributes. Default: \code{FALSE}.
 #' @returns list with the arguments of this function as list elements
 #' @export
+#' @examples
+#' inputs <- kwb.rabimo::rabimo_inputs_2025
+#' result <- run_rabimo(inputs$data, inputs$config)
 define_controls <- function(
     check = TRUE,
     use_abimo_bagrov_solver = TRUE,
