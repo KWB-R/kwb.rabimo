47de0f5dad35b21e9dd4f9c2c2035edf5713a1b4
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2025-04-04 19:13:04 +0200

    Rename test_api() to test_plumber_api()

--- R/test_plumber_api.R
+++ R/test_plumber_api.R
@@ -1,7 +1,7 @@
 #' Test the API to main functions using the plumber package and swagger
 #'
 #' @export
-test_api <- function()
+test_plumber_api <- function()
 {
   plumber_file <- system.file("scripts/plumber.R", package = "kwb.rabimo")
   plumber::pr_run(plumber::pr(plumber_file))
