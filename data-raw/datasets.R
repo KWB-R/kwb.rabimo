798961dc30a06546fbe8d6b54953a12dcffbd4e0
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2025-04-04 10:09:05 +0200

    Use xz compression

--- data-raw/datasets.R
+++ data-raw/datasets.R
@@ -1,4 +1,4 @@
 ## code to prepare datasets goes here
 
 rabimo_inputs_2025 <- readRDS("../../A/abimo.scripts/assets/rabimo_inputs_2025.rds")
-usethis::use_data(rabimo_inputs_2025, overwrite = TRUE)
+usethis::use_data(rabimo_inputs_2025, overwrite = TRUE, compress = "xz")
