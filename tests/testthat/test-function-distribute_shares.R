c4e8e4b0856d3b99f3b7e15e949844c4d5b84a61
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2025-04-08 17:02:35 +0200

    Implement tests, e.g. for cases reported by TSB

--- tests/testthat/test-function-distribute_shares.R
+++ tests/testthat/test-function-distribute_shares.R
@@ -1,5 +1,24 @@
-#library(testthat)
+# library(testthat)
 test_that("distribute_shares() works", {
+
   f <- kwb.rabimo:::distribute_shares
+
   expect_error(f())
+
+  result <- f(
+    partial_areas = 1:10,
+    base_areas = 2:11,
+    complementary_areas = 3:12,
+    target_value = 1
+  )
+
+  expect_true(is.data.frame(result))
+
+  expect_identical(
+    names(result),
+    c("area", "fraction", "consider", "ref_area", "delta")
+  )
+
+  expect_identical(nrow(result), 10L)
+
 })
