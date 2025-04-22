ed607a2c5d4dd19e6433d2c7a4e924673966aaa3
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2025-04-22 18:12:05 +0200

    Explicitly set encoding to UTF-8 in crs string
    and update the data stored in the package
    in order ot get rid of R CMD Check warning

--- data-raw/datasets.R
+++ data-raw/datasets.R
@@ -3,11 +3,13 @@
 # do you want to use georeferenced data?
 use_shape <- TRUE
 
-if(use_shape){
-  rabimo_inputs_2025 <- readRDS("../abimo.scripts/assets/rabimo_inputs_2025_geo.rds")
-  sf::st_as_sf(rabimo_inputs_2025$data)
+rabimo_inputs_2025 <- if (use_shape) {
+  inputs <- readRDS("../abimo.scripts/assets/rabimo_inputs_2025_geo.rds")
+  inputs$data <- sf::st_as_sf(inputs$data)
+  Encoding(attr(inputs$data$Shape, "crs")$wkt) <- "UTF-8"
+  inputs
 } else {
-  rabimo_inputs_2025 <- readRDS("../abimo.scripts/assets/rabimo_inputs_2025.rds")
+  readRDS("../abimo.scripts/assets/rabimo_inputs_2025.rds")
 }
 
 usethis::use_data(rabimo_inputs_2025, overwrite = TRUE, compress = "xz")
