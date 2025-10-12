a027f640fcbebfaae2065b91c678893996b8c89b
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2025-10-12 07:55:01 +0200

    Use some globals to shorten the main code

--- tests/testthat/test-function-run_rabimo_with_measures.R
+++ tests/testthat/test-function-run_rabimo_with_measures.R
@@ -1,7 +1,13 @@
 #library(testthat)
 
-features <- jsonlite::fromJSON('
-    [
+# Define globals
+{
+  FEATURES <- kwb.rabimo:::check_or_convert_data_types(
+    types = kwb.rabimo:::get_expected_data_type(), 
+    convert = TRUE, 
+    dbg = FALSE, 
+    data = jsonlite::fromJSON(
+      '[
       {
         "code": "0000000001000016",
         "prec_yr": 632,
@@ -50,7 +56,7 @@ features <- jsonlite::fromJSON('
         "total_area": 4951.8538,
         "area_main": 4951.8538,
         "area_rd": 0,
-        "main_frac": 1.0,
+        "main_frac": 1,
         "roof": 0.009,
         "green_roof": 0,
         "swg_roof": 1,
@@ -78,94 +84,55 @@ features <- jsonlite::fromJSON('
         "irrigation": 0,
         "block_type": "300_road"
       }
-    ]')
-
-features <- kwb.rabimo:::check_or_convert_data_types(
-  features,
-  types = kwb.rabimo:::get_expected_data_type(),
-  convert = TRUE,
-  dbg = FALSE
-)
+    ]'
+    )
+  )
+  
+  SAFETY_FACTOR <- 0.999
+  RUN <- kwb.rabimo::run_rabimo_with_measures
+  RUN_OLD <- function(...) RUN(..., old_version = TRUE, silent = TRUE)
+  GET_MAX <- function(x) lapply(kwb.rabimo:::get_measure_stats(x), `[[`, "max")
+  APPLY_MEASURES <- kwb.rabimo:::apply_measures_to_blocks
+  DATASETS <- lapply(
+    X = list(
+      d2020 = kwb.rabimo::rabimo_inputs_2020$data,
+      d2025 = kwb.rabimo::rabimo_inputs_2025$data
+    ), 
+    FUN = function(df) {
+      df[sample(seq_len(nrow(df)), 10L), ]
+    }
+  )
+  ADD_DELTA <- function(x, element, delta) {
+    x[[element]] <- x[[element]] + 0.01
+    x
+  }
+}
 
 test_that("run_rabimo_with_measures(old_version = TRUE) works", {
   
-  run_rabimo_with_measures <- kwb.rabimo::run_rabimo_with_measures
+  expect_error(RUN())
   
-  expect_error(run_rabimo_with_measures())
-  
-  test_me <- function(data) {
-    blocks <- data[sample(seq_len(nrow(data)), 10L), ]
-    stats <- kwb.rabimo:::get_measure_stats(blocks)
-    safety_factor <- 0.999
+  for (blocks in DATASETS) {
     
-    measures_max <- list(
-      green_roof = safety_factor * stats$green_roof$max,
-      unpaved = safety_factor * stats$unpaved$max,
-      to_swale = safety_factor * stats$to_swale$max
-    )
+    #blocks <- DATASETS$d2020
+    m_max <- as.list(SAFETY_FACTOR * unlist(GET_MAX(blocks)))
     
-    measures_too_big_1 <- list(
-      green_roof = measures_max$green_roof + 0.01,
-      unpaved = measures_max$unpaved,
-      to_swale = measures_max$to_swale
-    )
-    
-    measures_too_big_2 <- list(
-      green_roof = measures_max$green_roof,
-      unpaved = measures_max$unpaved + 0.01,
-      to_swale = measures_max$to_swale
-    )
-    
-    measures_too_big_3 <- list(
-      green_roof = measures_max$green_roof,
-      unpaved = measures_max$unpaved,
-      to_swale = measures_max$to_swale + 0.01
-    )
-    
-    expect_output(result <- run_rabimo_with_measures(
-      blocks, measures = measures_max, old_version = TRUE
-    ))
-    
+    expect_no_error(result <- RUN_OLD(blocks, measures = m_max))
     expect_true(all(result$surface_runoff == 0))
     
-    expect_error(run_rabimo_with_measures(
-      blocks, measures = measures_too_big_1, old_version = TRUE
-    ))
+    expect_error(RUN_OLD(blocks, measures = ADD_DELTA(m_max, "green_roof")))
+    expect_error(RUN_OLD(blocks, measures = ADD_DELTA(m_max, "unpaved")))
+    expect_error(RUN_OLD(blocks, measures = ADD_DELTA(m_max, "to_swale")))
     
-    expect_error(run_rabimo_with_measures(
-      blocks, measures = measures_too_big_2, old_version = TRUE
-    ))
-    
-    expect_error(run_rabimo_with_measures(
-      blocks, measures = measures_too_big_3, old_version = TRUE
-    ))
-  }
+  } # end of for (data in DATASETS)
   
-  test_me(data = kwb.rabimo::rabimo_inputs_2020$data)
-  test_me(data = kwb.rabimo::rabimo_inputs_2025$data)
-  
-  expect_no_error(expect_output(
-    kwb.rabimo::run_rabimo_with_measures(
-      features, 
-      measures = list(green_roof = 0.009, to_swale = 0, unpaved = 0.3), 
-      old_version = TRUE
-    )
-  ))
-  
-  expect_error(
-    kwb.rabimo::run_rabimo_with_measures(
-      features, 
-      measures = list(green_roof = 0.00900001, to_swale = 0, unpaved = 0.3),
-      old_version = TRUE
-    )
-  )
+  measures <- list(green_roof = 0.009, to_swale = 0, unpaved = 0.3)
+  expect_no_error(RUN_OLD(FEATURES, measures = measures))
+  expect_error(RUN_OLD(FEATURES, measures = ADD_DELTA(measures, "green_roof")))
+
 })
 
 test_that("Full connection to swales results in zero runoff", {
-
-  generate <- kwb.rabimo::generate_rabimo_area
-  get_stats <- kwb.rabimo:::get_measure_stats
-  apply_measures <- kwb.rabimo:::apply_measures_to_blocks
   
   run <- function(blocks, measures) {
     kwb.rabimo::run_rabimo_with_measures(
@@ -177,7 +144,7 @@ test_that("Full connection to swales results in zero runoff", {
   }
   
   # different versions of sealed = 0.3
-  blocks <- generate(
+  blocks <- kwb.rabimo::generate_rabimo_area(
     code = as.character(1:3), 
     roof = c(0.0, 0.1, 0.2), 
     pvd  = c(0.3, 0.2, 0.1)
@@ -186,16 +153,15 @@ test_that("Full connection to swales results in zero runoff", {
   measures <- list(green_roof = NA, unpaved = NA, to_swale = 0.3)
   result <- run(blocks, measures)
   expect_true(all(result$runoff == 0))
-  
-  # max. green_roof = mean(roof) = 0.1
-  max_green_roof <- get_stats(blocks)$green_roof$max
-  # max. unpaved = mean(1 - roof) = 0.9
-  max_unpaved <- get_stats(blocks)$unpaved$max
-  # max. to_swale = mean(roof) = 0.1 (in case of max. removal of pavement)
-  max_to_swale <- get_stats(
-    apply_measures(blocks, global_share_unpaved = max_unpaved)
-  )$to_swale$max
 
+  # max. green_roof = mean(roof) = 0.1
+  # max. unpaved = mean(1 - roof) = 0.9
+  m_max <- GET_MAX(blocks)
+  # correct max. to_swale
+  m_max$to_swale <- GET_MAX(
+    APPLY_MEASURES(blocks, global_share_unpaved = m_max$unpaved)
+  )$to_swale
+  
   measures <- list(green_roof = 0.1, unpaved = 0.9, to_swale = 0.1)
   result <- run(blocks, measures)
   expect_true(all(result$runoff == 0))
